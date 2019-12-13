caa892249d9ad10b13e317e6caeb2a69eb7f991d
Lees-Oz <lev.guzeev@gmail.com>
2019-12-13 01:20:52 +0100

    better instruction reading

--- src/main/scala/Day2.scala
+++ src/main/scala/Day2.scala
@@ -1,4 +1,4 @@
-object Day2 {
+object Day2 extends App {
 
   case class State(memory: List[Int], pointer: Int)
 
@@ -9,27 +9,19 @@ object Day2 {
   case class Mul(par1: Int, par2: Int, to: Int) extends Instruction(4)
 
   object Instruction {
-    def apply(memory: List[Int]): Either[Throwable, Instruction] = for {
-      opcode <- memory.headOption.toRight(new Exception("Empty memory"))
-      instruction <- opcode match {
-        case code @ (1 | 2) =>
-          memory.slice(1, 4) match {
-            case pars @ List(par1, par2, par3) =>
-              if (code == 1) Right(Add(par1, par2, par3))
-              else Right(Mul(par1, par2, par3))
-            case _ =>
-              Left(new Exception("Not enough parameters"))
-          }
-        case 99 => Right(Halt)
+    def read(from: List[Int]): Either[Throwable, Instruction] =
+      from match {
+        case List(1, p1, p2, p3, _*) => Right(Add(p1, p2, p3))
+        case List(2, p1, p2, p3, _*) => Right(Mul(p1, p2, p3))
+        case List(99, _*) => Right(Halt)
         case _ => Left(new Exception("Invalid instruction"))
       }
-    } yield instruction
   }
 
   def intcode(program: List[Int]): Either[Throwable, State] = {
     def run(state: State): Either[Throwable, State] =
       for {
-        instruction <- Instruction(state.memory.drop(state.pointer))
+        instruction <- Instruction.read(state.memory.drop(state.pointer))
         result <- instruction match {
           case Halt => Right(state)
           case Add(op1, op2, res) => run(State(
@@ -45,6 +37,8 @@ object Day2 {
   }
 
   val input = List(1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,2,9,19,23,2,13,23,27,1,6,27,31,2,6,31,35,2,13,35,39,1,39,10,43,2,43,13,47,1,9,47,51,1,51,13,55,1,55,13,59,2,59,13,63,1,63,6,67,2,6,67,71,1,5,71,75,2,6,75,79,1,5,79,83,2,83,6,87,1,5,87,91,1,6,91,95,2,95,6,99,1,5,99,103,1,6,103,107,1,107,2,111,1,111,5,0,99,2,14,0,0)
-  val result: Either[Throwable, Int] = intcode(input).map(_.memory.head)
+  val result: Either[Throwable, Int] = intcode(input
+    .patch(1, Seq(12), 1)
+    .patch(2, Seq(2), 1)).map(_.memory.head)
   println(result)
 }
