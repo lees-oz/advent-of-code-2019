995921ddfe5c97c324ebe713cd0378c021295b07
Lees-Oz <lev.guzeev@gmail.com>
2020-01-03 01:23:17 +0100

    Day5 part 2

--- src/main/scala/day5/Day5.scala
+++ src/main/scala/day5/Day5.scala
@@ -9,122 +9,115 @@ object Day5 extends App {
   case object Halt extends Instruction
   case class Add(par1: Int, par2: Int, to: Int) extends Instruction
   case class Mul(par1: Int, par2: Int, to: Int) extends Instruction
-  case class In(what: Int, to: Int) extends Instruction
-  case class Out(what: Int) extends Instruction
+  case class Input(what: Int, to: Int) extends Instruction
+  case class Output(what: Int) extends Instruction
+  case class JumpIfTrue(sub: Int, goto: Int) extends Instruction
+  case class JumpIfFalse(sub: Int, goto: Int) extends Instruction
+  case class LessThan(left: Int, right: Int, to: Int) extends Instruction
+  case class Equals(left: Int, right: Int, to: Int) extends Instruction
 
-  implicit val decoder: Decoder[Instruction] = (code: Code) => {
-    code.opcode match {
-      case 1 =>
-        for {
-          par1 <- MemParam(code, 0)
-          par2 <- MemParam(code, 1)
-          to <- ByVal(code, 2)
-        } yield Add(par1, par2, to)
-      case 2 =>
-        for {
-          par1 <- MemParam(code, 0)
-          par2 <- MemParam(code, 1)
-          to <- ByVal(code, 2)
-        } yield Mul(par1, par2, to)
-      case 3 =>
-        for {
-          what <- Input()
-          to <- ByVal(code, 0)
-        } yield In(what, to)
-      case 4     => ByRef(code, 0).map(Out.apply)
-      case 99    => IO.pure(Halt)
-      case i @ _ => IO.raiseError(new Exception(s"Unknown instruction $i"))
-    }
-  }
+  object implicits {
 
-  implicit val executor = new Executor[Instruction, ConsoleOutput] {
-    override def execute(
-      instruction: Instruction
-    )(state: State[ConsoleOutput]): IO[ExecutionResult[ConsoleOutput]] = {
-      instruction match {
-        case Add(par1, par2, to) =>
+    implicit val decoder: Decoder[Instruction] = (s: State[_]) => {
+      s.code.opcode match {
+        case 1 =>
           for {
-            newCodeDump <- IO { state.code.dump.updated(to, par1 + par2) }
-            newIp <- IO.pure(state.code.pointer + 4)
-          } yield ExecutionResult(State(Code(newCodeDump, newIp), state.data))
-        case Mul(par1, par2, to) =>
+            par1 <- MemParam(s.code, 0)
+            par2 <- MemParam(s.code, 1)
+            to <- ByVal(s.code, 2)
+          } yield Add(par1, par2, to)
+        case 2 =>
           for {
-            newCodeDump <- IO { state.code.dump.updated(to, par1 * par2) }
-            newIp <- IO.pure(state.code.pointer + 4)
-          } yield ExecutionResult(State(Code(newCodeDump, newIp), state.data))
-        case In(what, to) =>
+            par1 <- MemParam(s.code, 0)
+            par2 <- MemParam(s.code, 1)
+            to <- ByVal(s.code, 2)
+          } yield Mul(par1, par2, to)
+        case 3 =>
           for {
-            newCodeDump <- IO { state.code.dump.updated(to, what) }
-            newIp <- IO.pure(state.code.pointer + 2)
-          } yield ExecutionResult(State(Code(newCodeDump, newIp), state.data))
-        case Out(what) =>
+            what <- s.input
+            to <- ByVal(s.code, 0)
+          } yield Input(what, to)
+        case 4 => ByRef(s.code, 0).map(Output.apply)
+        case 5 =>
           for {
-            _ <- IO { println(what) }
-            newIp <- IO.pure(state.code.pointer + 2)
-            newData <- IO.pure(state.data + what)
-          } yield ExecutionResult(State(Code(state.code.dump, newIp), newData))
-        case Halt =>
-          IO.pure(ExecutionResult(State(state.code, state.data), true))
+            sub <- MemParam(s.code, 0)
+            goto <- MemParam(s.code, 1)
+          } yield JumpIfTrue(sub, goto)
+        case 6 =>
+          for {
+            sub <- MemParam(s.code, 0)
+            goto <- MemParam(s.code, 1)
+          } yield JumpIfFalse(sub, goto)
+        case 7 =>
+          for {
+            left <- MemParam(s.code, 0)
+            right <- MemParam(s.code, 1)
+            to <- ByVal(s.code, 2)
+          } yield LessThan(left, right, to)
+        case 8 =>
+          for {
+            left <- MemParam(s.code, 0)
+            right <- MemParam(s.code, 1)
+            to <- ByVal(s.code, 2)
+          } yield Equals(left, right, to)
+        case 99    => IO.pure(Halt)
+        case i @ _ => IO.raiseError(new Exception(s"Unknown instruction $i"))
       }
     }
+
+    type ConsoleOutput = String
+
+    implicit val executor: Executor[Instruction, ConsoleOutput] =
+      (instruction: Instruction, s: State[ConsoleOutput]) =>
+        instruction match {
+          case Add(par1, par2, to) =>
+            for {
+              dump <- IO(s.code.dump.updated(to, par1 + par2))
+              pointer <- IO.pure(s.code.pointer + 4)
+            } yield Result(State(Code(dump, pointer), s.data, s.input))
+          case Mul(par1, par2, to) =>
+            for {
+              dump <- IO(s.code.dump.updated(to, par1 * par2))
+              pointer <- IO.pure(s.code.pointer + 4)
+            } yield Result(State(Code(dump, pointer), s.data, s.input))
+          case Input(what, to) =>
+            for {
+              dump <- IO(s.code.dump.updated(to, what))
+              pointer <- IO.pure(s.code.pointer + 2)
+            } yield Result(State(Code(dump, pointer), s.data, s.input))
+          case Output(what) =>
+            for {
+              pointer <- IO.pure(s.code.pointer + 2)
+              data <- IO.pure(s.data + "\n" + what)
+            } yield Result(State(Code(s.code.dump, pointer), data, s.input))
+          case JumpIfTrue(sub, goto) =>
+            for {
+              pointer <- IO.pure { if (sub != 0) goto else s.code.pointer + 3 }
+            } yield Result(State(Code(s.code.dump, pointer), s.data, s.input))
+          case JumpIfFalse(sub, goto) =>
+            for {
+              pointer <- IO.pure { if (sub == 0) goto else s.code.pointer + 3 }
+            } yield Result(State(Code(s.code.dump, pointer), s.data, s.input))
+          case LessThan(left, right, to) =>
+            for {
+              pointer <- IO.pure(s.code.pointer + 4)
+              what <- IO.pure { if (left < right) 1 else 0 }
+              dump <- IO.pure { s.code.dump.updated(to, what) }
+            } yield Result(State(Code(dump, pointer), s.data, s.input))
+          case Equals(left, right, to) =>
+            for {
+              pointer <- IO.pure { s.code.pointer + 4 }
+              what <- IO.pure { if (left == right) 1 else 0 }
+            } yield
+              Result(
+                State(
+                  Code(s.code.dump.updated(to, what), pointer),
+                  s.data,
+                  s.input
+                )
+              )
+          case Halt =>
+            IO.pure(Result(State(s.code, s.data, s.input), true))
+      }
   }
-
-  type ConsoleOutput = String
-
-  def part1(input: List[Int]): State[ConsoleOutput] = {
-    IntCode5
-      .run(State(Code(input, 0), ""))
-      .unsafeRunSync()
-  }
-
-  val input = List(3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 1102, 16,
-    13, 225, 1001, 88, 68, 224, 101, -114, 224, 224, 4, 224, 1002, 223, 8, 223,
-    1001, 224, 2, 224, 1, 223, 224, 223, 1101, 8, 76, 224, 101, -84, 224, 224,
-    4, 224, 102, 8, 223, 223, 101, 1, 224, 224, 1, 224, 223, 223, 1101, 63, 58,
-    225, 1102, 14, 56, 224, 101, -784, 224, 224, 4, 224, 102, 8, 223, 223, 101,
-    4, 224, 224, 1, 223, 224, 223, 1101, 29, 46, 225, 102, 60, 187, 224, 101,
-    -2340, 224, 224, 4, 224, 102, 8, 223, 223, 101, 3, 224, 224, 1, 224, 223,
-    223, 1102, 60, 53, 225, 1101, 50, 52, 225, 2, 14, 218, 224, 101, -975, 224,
-    224, 4, 224, 102, 8, 223, 223, 1001, 224, 3, 224, 1, 223, 224, 223, 1002,
-    213, 79, 224, 101, -2291, 224, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 2,
-    224, 1, 223, 224, 223, 1, 114, 117, 224, 101, -103, 224, 224, 4, 224, 1002,
-    223, 8, 223, 101, 4, 224, 224, 1, 224, 223, 223, 1101, 39, 47, 225, 101, 71,
-    61, 224, 101, -134, 224, 224, 4, 224, 102, 8, 223, 223, 101, 2, 224, 224, 1,
-    224, 223, 223, 1102, 29, 13, 225, 1102, 88, 75, 225, 4, 223, 99, 0, 0, 0,
-    677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1105, 0, 99999, 1105, 227, 247, 1105,
-    1, 99999, 1005, 227, 99999, 1005, 0, 256, 1105, 1, 99999, 1106, 227, 99999,
-    1106, 0, 265, 1105, 1, 99999, 1006, 0, 99999, 1006, 227, 274, 1105, 1,
-    99999, 1105, 1, 280, 1105, 1, 99999, 1, 225, 225, 225, 1101, 294, 0, 0, 105,
-    1, 0, 1105, 1, 99999, 1106, 0, 300, 1105, 1, 99999, 1, 225, 225, 225, 1101,
-    314, 0, 0, 106, 0, 0, 1105, 1, 99999, 1107, 677, 677, 224, 102, 2, 223, 223,
-    1006, 224, 329, 1001, 223, 1, 223, 108, 677, 677, 224, 1002, 223, 2, 223,
-    1005, 224, 344, 101, 1, 223, 223, 1008, 226, 226, 224, 102, 2, 223, 223,
-    1006, 224, 359, 1001, 223, 1, 223, 1107, 226, 677, 224, 102, 2, 223, 223,
-    1006, 224, 374, 1001, 223, 1, 223, 8, 677, 226, 224, 102, 2, 223, 223, 1006,
-    224, 389, 101, 1, 223, 223, 8, 226, 226, 224, 102, 2, 223, 223, 1006, 224,
-    404, 101, 1, 223, 223, 7, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 419,
-    101, 1, 223, 223, 7, 677, 226, 224, 1002, 223, 2, 223, 1005, 224, 434, 101,
-    1, 223, 223, 1108, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 449, 1001,
-    223, 1, 223, 108, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 464, 101, 1,
-    223, 223, 1108, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 479, 101, 1,
-    223, 223, 1007, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 494, 1001, 223,
-    1, 223, 107, 226, 226, 224, 102, 2, 223, 223, 1005, 224, 509, 1001, 223, 1,
-    223, 1008, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 524, 1001, 223, 1,
-    223, 1007, 226, 226, 224, 102, 2, 223, 223, 1006, 224, 539, 101, 1, 223,
-    223, 1108, 677, 677, 224, 102, 2, 223, 223, 1005, 224, 554, 1001, 223, 1,
-    223, 1008, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 569, 101, 1, 223,
-    223, 1107, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 584, 1001, 223, 1,
-    223, 7, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 599, 101, 1, 223, 223,
-    108, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 614, 101, 1, 223, 223,
-    107, 226, 677, 224, 1002, 223, 2, 223, 1005, 224, 629, 1001, 223, 1, 223,
-    107, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 644, 101, 1, 223, 223,
-    1007, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 659, 101, 1, 223, 223, 8,
-    226, 677, 224, 102, 2, 223, 223, 1005, 224, 674, 1001, 223, 1, 223, 4, 223,
-    99, 226)
-
-  val res = IntCode5
-    .run(State(Code(input, 0), ""))
-    .unsafeRunSync()
-
-  println(res)
 }
--- src/main/scala/day5/IntCode5.scala
+++ src/main/scala/day5/IntCode5.scala
@@ -10,26 +10,26 @@ object IntCode5 {
 
     private def modes: Int = current.head / 100
 
+    def mode(n: Int): Int = modes % tenPower(n + 1) / tenPower(n)
+
     val current: List[Int] = dump.drop(pointer)
 
     val opcode: Int = current.head % 100
-
-    def mode(n: Int): Int = modes % tenPower(n + 1) / tenPower(n)
   }
 
-  case class State[D](code: Code, data: D)
+  case class State[D](code: Code, data: D, input: IO[Int])
 
   trait Decoder[A] {
-    def decode(code: Code): IO[A]
+    def decode(s: State[_]): IO[A]
   }
   object Decoder {
     def apply[A](implicit d: Decoder[A]): Decoder[A] = implicitly[Decoder[A]]
   }
 
-  case class ExecutionResult[D](state: State[D], halt: Boolean = false)
+  case class Result[D](state: State[D], halt: Boolean = false)
 
   trait Executor[I, D] {
-    def execute(instruction: I)(state: State[D]): IO[ExecutionResult[D]]
+    def execute(instruction: I, state: State[D]): IO[Result[D]]
   }
   object Executor {
     def apply[I, D](implicit executor: Executor[I, D]): Executor[I, D] =
@@ -47,12 +47,12 @@ object IntCode5 {
   }
 
   object ByVal {
-    def apply(code: Code, n: Int): IO[Int] = IO { code.current.tail(n) }
+    def apply(code: Code, n: Int): IO[Int] = IO { code.current(n + 1) }
   }
 
   object ByRef {
     def apply(code: Code, n: Int): IO[Int] = IO {
-      code.dump(code.current.tail(n))
+      code.dump(code.current(n + 1))
     }
   }
 
@@ -64,8 +64,8 @@ object IntCode5 {
     state: State[D]
   )(implicit e: Executor[I, D]): IO[State[D]] =
     for {
-      instruction <- Decoder[I].decode(state.code)
-      execResult <- Executor[I, D].execute(instruction)(state)
+      instruction <- Decoder[I].decode(state)
+      execResult <- Executor[I, D].execute(instruction, state)
       result <- {
         if (execResult.halt) IO.pure(execResult.state)
         else run(execResult.state)
