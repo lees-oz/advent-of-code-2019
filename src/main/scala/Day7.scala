95a9bd61855f80dc1b8813dbb7f12ca59b9367c3
Lev Guzeev <lev.guzeev@gmail.com>
2020-01-03 18:25:07 +0100

    7.1 using native permutations function on list

--- src/main/scala/Day7.scala
+++ src/main/scala/Day7.scala
@@ -4,26 +4,20 @@ import day5.IntCode5._
 import day5.Day5.implicits._
 
 object Day7 extends {
-  private def combinations(s: Set[Int], acc: List[Int] = Nil): Set[List[Int]] = {
-    if (s.isEmpty) Set(acc)
-    else s.flatMap(i => combinations(s - i, i :: acc))
-  }
-
   private def runAmps(program: List[Int]): IO[Int] = {
     def runAmp(code: IntCode5.Code, phaseSettings: List[Int], prevOut: Int = 0): IO[Int] = {
-      if(phaseSettings.isEmpty) IO.pure(prevOut)
+      if (phaseSettings.isEmpty) IO.pure(prevOut)
       else IntCode5.run(State(code, List(phaseSettings.head, prevOut), Nil)).flatMap(out =>
         runAmp(code, phaseSettings.tail, out.head))
     }
 
     import cats.implicits._
-    combinations((0 to 4).toSet)
+    (0 to 4).toList
+      .permutations
       .toList
-      .traverse(c => runAmp(Code(program, 0), c))
+      .traverse(p => runAmp(Code(program, 0), p))
       .map(_.max)
   }
 
-  def part1(program: List[Int]): Int = {
-    runAmps(program).unsafeRunSync()
-  }
+  def part1(program: List[Int]): Int = runAmps(program).unsafeRunSync()
 }
